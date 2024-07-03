584c64dd6bb3009f9d5ca8fbc973725278f32bfc
Jonghwi Kim <54308894+bellhwi@users.noreply.github.com>
2024-07-03 13:40:31 -0700

    add form submission page

--- script.js
+++ script.js
@@ -92,3 +92,27 @@ document.getElementById('dark-mode-toggle').addEventListener('click', () => {
     ? enableDarkMode()
     : disableDarkMode()
 })
+
+// Handle form submission
+const handleSubmit = (event) => {
+  event.preventDefault()
+
+  const myForm = event.target
+  const formData = new FormData(myForm)
+
+  fetch('/', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: new URLSearchParams(formData).toString(),
+  })
+    .then((response) => {
+      if (response.ok) {
+        window.open('/thank-you.html', '_blank')
+      } else {
+        throw new Error('Form submission failed')
+      }
+    })
+    .catch((error) => alert(error))
+}
+
+document.querySelector('form').addEventListener('submit', handleSubmit)
